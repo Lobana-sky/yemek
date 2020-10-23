db02adcb7dc70bba19221b7115a2ecd1e77b9233
Lobana-sky <loubanaeng@gmail.com>
2020-10-23 16:23:55 +0300

    white-space:nowrap

--- src/container/home.jsx
+++ src/container/home.jsx
@@ -9,7 +9,7 @@ function home() {
     <div className="home-style">
       <Header></Header>
       <div className="foodtypes">
-      {[1,2,3,4,5,6,7,8,9].map(()=><a href="#"><Foodtype></Foodtype></a>)}
+      {[1,2,3,4,5,6,7,8,9,10,11,12].map(()=><a href="#"><Foodtype></Foodtype></a>)}
       </div>
     </div>
   );
