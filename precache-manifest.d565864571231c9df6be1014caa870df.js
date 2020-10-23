0fbe5da1676ff89c4eaac6901aed1959376d4824
Lobana-sky <loubanaeng@gmail.com>
2020-10-23 17:30:02 +0300

    Updates

---
+++ precache-manifest.d565864571231c9df6be1014caa870df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb0744e550c77f93d38413b18ce2db1",
+    "url": "/yemek/index.html"
+  },
+  {
+    "revision": "e7522d06214efa989eb8",
+    "url": "/yemek/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "458bbc1e3bd87e0c895a",
+    "url": "/yemek/static/css/main.847ca5f4.chunk.css"
+  },
+  {
+    "revision": "e7522d06214efa989eb8",
+    "url": "/yemek/static/js/2.40e287a0.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/yemek/static/js/2.40e287a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458bbc1e3bd87e0c895a",
+    "url": "/yemek/static/js/main.f931bb1b.chunk.js"
+  },
+  {
+    "revision": "535d209aedecc21ef740",
+    "url": "/yemek/static/js/runtime-main.a31e20b4.js"
+  }
+]);
