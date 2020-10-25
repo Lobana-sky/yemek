41a1f00f27c17b762472dbca6a640fdba1cf2006
Lobana-sky <loubanaeng@gmail.com>
2020-10-26 00:36:05 +0300

    install npm and import awesom icon for cart shopping

--- src/component/foodtypedetails.jsx
+++ src/component/foodtypedetails.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Image} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import '../App.css';
 
 function foodtypedetails({img,name,price}) {
@@ -13,7 +15,7 @@ function foodtypedetails({img,name,price}) {
         <div className="div-details">
         <p className="food-detail-name align-self-start">{name}</p>
         <p className="food-detail-price align-self-end">${price}</p>
-        <button className="sell-btn">Warning</button>
+        <button className="sell-btn"><FontAwesomeIcon icon={faCoffee} /></button>
         </div>
        
     </div>
